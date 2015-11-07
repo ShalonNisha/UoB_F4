ef459d3219ae877970689a6a334e71ffe8851556
Nishan <nishan4f@hotmail.com>
2015-11-07 17:09:38 +0000

    Load countries implemented

---
+++ tag_trends/parser/Country.java
@@ -0,0 +1,56 @@
+package tag_trends.parser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Country {
+
+	public static void parse(String fileName) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File(
+					fileName)));
+			String line = null;
+			Map<String, Integer> countriesData = new HashMap<String, Integer>();
+
+			while ((line = br.readLine()) != null) {
+				String[] data = line.split(",");
+				countriesData.put(data[1], Integer.parseInt(data[0]));
+			}
+
+			br.close();
+
+			ObjectOutputStream oos = new ObjectOutputStream(
+					new FileOutputStream("countries.dat"));
+			oos.writeObject(countriesData);
+			oos.flush();
+			oos.close();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static Map<String, Integer> loadCountries() {
+		ObjectInputStream oos;
+		Map<String, Integer> countriesData = null;
+		try {
+			oos = new ObjectInputStream(new FileInputStream("countries.dat"));
+			countriesData = (Map<String, Integer>) oos.readObject();
+			oos.close();
+			System.out.println(countriesData);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return countriesData;
+	}
+}
